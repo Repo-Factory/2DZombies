269152a50c1d02297efdf46f2737ffc27bb95f35
Conner <conner.sommerfield@gmail.com>
2023-10-20 21:34:04 -0700

    Fix Collisions

---
+++ Assets/Projectiles/Scripts/Grape.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grape : Projectile
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        damage = 10.0f;
+        speed = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Projectiles/Scripts/Projectile.cs
+++ Assets/Projectiles/Scripts/Projectile.cs
@@ -6,24 +6,25 @@ public class Projectile : MonoBehaviour
 {
     public const string ENEMY_NAME = "Enemy";
     public const int NO_HEALTH = 0;
-    public const int ENEMY_LINE = 16;
+    public const int ENEMY_LINE = 4;
     public float damage;
     public float speed;
 
     void Update()
     {
         Vector3 position = new Vector3((float)transform.position.x, (float)transform.position.y, (float)transform.position.z);
-        position.z += (speed * (float)Time.deltaTime);
+        position.y += (speed * (float)Time.deltaTime);
         transform.position = position;
 
-        if (this.transform.position.z >= ENEMY_LINE)
+        if (this.transform.position.y >= ENEMY_LINE)
         {
             Destroy(this.gameObject);
         }
     }
 
-    void OnTriggerEnter(Collider collision)
-    { 
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger!");
         if (collision.gameObject.CompareTag(ENEMY_NAME))
         {
             GameObject zombie = collision.gameObject;
